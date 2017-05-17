b00e675229ebae9197a5889294dcacc1cf0a7474
cbedge <cbedge@gmail.com>
2017-05-17 09:11:26 -0400

    Corrections to incubation
    Return ND if the stage is not reached by the specified day.
    
    Code now runs through all rows in the table

--- Incubation.R
+++ Incubation.R
@@ -2,23 +2,29 @@
 ###Currently in development, things work but they are slow###
 
 
-setwd("C:/Users/christopher.edge/Documents/GitHub/Turtles/Data") #set the working directory to where your data are
-nestdate <- read.csv(file = "nestdate.csv", header=T) #File with turtle ID and nest dates
-develop <- read.csv(file = "develop.csv", header=T) #File with daily development rate
+setwd("C:/Users/christopher.edge/Documents/Github/Turtle_Development/data") #set the working directory to where your data are
+nestdate <- read.csv(file = "nestdate_M.csv", header=T) #File with turtle ID and nest dates
+develop <- read.csv(file = "develop_M.csv", header=T) #File with daily development rate
 names(develop)
 names(nestdate)
 
 #calculate date at various stages
-for (i in 1:length(nestdate)){
-  #calculate date at stage 6
+for (i in 1:length(nestdate$turtleID)){
   a<-nestdate$nestdate[i] #store the nest date
   b<-nestdate$nestyear[i] #store the nest year
   current.sum <- 0
   while (current.sum <=6) {
     current.sum <- current.sum + develop$dev[develop$julian==a & develop$year==b]
     a <- a+1
+    if(a==274){
+      current.sum <-50
+    }
   }
-  nestdate$dateat6[i] <- a-1
+  if(current.sum==50){
+    nestdate$dateat6[i] <- "ND"
+  }else{
+    nestdate$dateat6[i] <- a-1
+  }
   
   #calculate date at stage 11
   a<-nestdate$nestdate[i] #store the nest date
@@ -26,22 +32,35 @@ for (i in 1:length(nestdate)){
   while (current.sum <=11) {
     current.sum <- current.sum + develop$dev[develop$julian==a & develop$year==b]
     a <- a+1
+    if(a==274){
+      current.sum <-50
+    }
   }
-  nestdate$dateat11[i] <- a-1
+  if(current.sum==50){
+    nestdate$dateat11[i] <- "ND"
+  }else{
+    nestdate$dateat11[i] <- a-1
+  }
 
   #calculate date at stage 20
   a<-nestdate$nestdate[i] #store the nest date
   current.sum <- 0
   while (current.sum <=20) {
     current.sum <- current.sum + develop$dev[develop$julian==a & develop$year==b]
-    a <- a+1
+   a <- a+1
+   if(a==274){
+     current.sum <-50
+   }
   }
-  nestdate$dateat20[i] <- a-1
-  
+  if(current.sum==50){
+    nestdate$dateat20[i] <- "ND"
+  }else{
+    nestdate$dateat20[i] <- a-1
+  }
 }
 
 #calculate stage at various days
-for (i in (1:length(nestdate))){
+for (i in (1:length(nestdate$turtleID))){
   nestdate$stageat244[i] <- sum(develop[develop$year==nestdate$nestyear[i] & develop$julian >= nestdate$nestdate[i] & develop$julian <= 244,]$dev)
   nestdate$stageat258[i] <- sum(develop[develop$year==nestdate$nestyear[i] & develop$julian >= nestdate$nestdate[i] & develop$julian <= 258,]$dev)
   nestdate$stageat273[i] <- sum(develop[develop$year==nestdate$nestyear[i] & develop$julian >= nestdate$nestdate[i] & develop$julian <= 273,]$dev)
